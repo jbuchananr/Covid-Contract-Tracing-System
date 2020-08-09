e60936547003c3a3c726119293f18a5b6186edb9
Jonathan Buchanan <jbuchananr19@gmail.com>
2020-08-08 21:03:37 -0400

    Last Commit

--- swipe.py
+++ swipe.py
@@ -7,18 +7,7 @@ from tkinter import ttk
 from functools import partial
 import simpleaudio as sa
 from time import strftime 
-cfg = {
-    "apiKey": "AIzaSyClZ_r-O4VkFsRvQy00mK1tbktR1qnHBGo",
-    "authDomain": "zbtscanner.firebaseapp.com",
-    "databaseURL": "https://zbtscanner.firebaseio.com",
-    "projectId": "zbtscanner",
-    "storageBucket": "zbtscanner.appspot.com",
-    "messagingSenderId": "848484055790",
-    "appId": "1:848484055790:web:c60a43dba75a6bf10fd1ff"
-}
-superuseremail = "jbuchananr19@gmail.com"
-adminemail = "admin@zbt.com"
-# from secret import cfg, superuseremail, adminemail
+from secret import cfg, superuseremail, adminemail
 # Constants
 kZBTYellow = '#ffd203'
 kZBTBlue = '#121245' 
